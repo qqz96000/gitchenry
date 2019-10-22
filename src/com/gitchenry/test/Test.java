cff7e88bed2ac5e43035f117993bcca1516f40b2
qqz96000 <599091053@qq.com>
2019-10-22 11:39:51 +0800

    修改

--- src/com/gitchenry/test/Test.java
+++ src/com/gitchenry/test/Test.java
@@ -2,6 +2,6 @@ package com.gitchenry.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("1dsf111dd1");
+        System.out.println("1ds1");
     }
 }
